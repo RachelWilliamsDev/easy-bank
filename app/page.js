5a44266af635ed1ba415a53b99a4660bb357cf9f
RachelWilliamsDev <Rachel@stacktiger.co>
2024-11-04 15:11:38 +0000

    feat: why choose easybank section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
+import WhyChooseEasybank from "@/components/WhyChooseEasybank";
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full bg-white">
       <NavBar />
       <Header />
+      <WhyChooseEasybank />
     </main>
   );
 }
